c64581e2db445c8644b794f13ecfcc81551da17b
Narasimha Reddy Boggla <bnry656@gmail.com>
2026-08-24 20:01:51 +0530

    Add user input

--- rps_game.py
+++ rps_game.py
@@ -3,3 +3,6 @@ print("Rules:")
 print("Rock beats Scissors")
 print("Scissors beats Paper")
 print("Paper beats Rock")
+
+user_cholice = input("Enter rock , paper, or scissors:")
+print("You chose:",user_choice)

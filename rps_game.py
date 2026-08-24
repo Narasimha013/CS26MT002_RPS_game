051ad854de838442c2558197ea5f720f094ec2de
Narasimha Reddy Boggla <bnry656@gmail.com>
2026-08-24 20:11:12 +0530

    Rematch on tie

--- rps_game.py
+++ rps_game.py
@@ -4,7 +4,7 @@ print("Rock beats Scissors")
 print("Scissors beats Paper")
 print("Paper beats Rock")
 
-user_cholice = input("Enter rock , paper, or scissors:")
+user_choice = input("Enter rock , paper, or scissors:")
 print("You chose:",user_choice)
 
 import random 
@@ -15,10 +15,18 @@ computer_choice = random.choice(choices)
 print("Computer chose:",computer_choice)
 
 if user_choice == computer_choice:
-    print("It's a tie!")
-elif (user_choice == "rock" and computer_choice == "scissors") or \
-     (user_choice == "scissors" and computer_choice == "paper") or \
-     (user_choice == "paper" and computer_choice == "rock"):
-    print("You win!")
-else:
-    print("Computer wins!")
+   while True:
+    computer_choice = random.choice(choices)
+    print("Computer chose:", computer_choice)
+
+    if user_choice == computer_choice:
+        print("It's a tie! Rematching...")
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "scissors" and computer_choice == "paper") or \
+         (user_choice == "paper" and computer_choice == "rock"):
+        print("You win!")
+        break
+    else:
+        print("Computer wins!")
+        break
+

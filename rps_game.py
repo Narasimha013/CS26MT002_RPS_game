8f3ec42acf2df4fdd86b943d056b2675badc2db8
Narasimha Reddy Boggla <bnry656@gmail.com>
2026-08-24 20:05:52 +0530

    Add computer random selection

--- rps_game.py
+++ rps_game.py
@@ -6,3 +6,10 @@ print("Paper beats Rock")
 
 user_cholice = input("Enter rock , paper, or scissors:")
 print("You chose:",user_choice)
+
+import random 
+
+cholices = ["rock","paper","scissors"]
+computer_choice = random.choice(choices)
+
+print("Computer chose:",computer_choice)
